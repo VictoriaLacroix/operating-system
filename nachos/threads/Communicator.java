5ca7eb66e1ba07d0481f85a1736a890e60736c77
egelinas <egelinas@laurentian.ca>
2016-02-19 16:09:54 -0500

    Implemented first pass solution for Communicator +first past solution code

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -2,40 +2,71 @@ package nachos.threads;
 
 import nachos.machine.*;
 
-/**
- * A <i>communicator</i> allows threads to synchronously exchange 32-bit
- * messages. Multiple threads can be waiting to <i>speak</i>,
- * and multiple threads can be waiting to <i>listen</i>. But there should never
- * be a time when both a speaker and a listener are waiting, because the two
- * threads can be paired off at this point.
- */
-public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
-    public Communicator() {
-    }
+public class Communicator{
+	
+	private int message;
+	
+	Lock ourLock;
+	Condition2 toSpeak;
+	Condition2 toListen;
+	int listenerWaiting = 0;
+	int speakerWaiting = 0;
+	int speakerActive = 0;
+	int listenerActive = 0;
+	boolean messageActive = false;
+	
+	public Communicator(){
+		ourLock = new Lock();
+		toSpeak = new Condition2(ourLock);
+		toListen = new Condition2(ourLock);
+	}
 
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
-    public void speak(int word) {
-    }
 
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	return 0;
-    }
+	public void speaker(int word){
+		ourLock.acquire();	
+		speakerWaiting++;
+		
+		while(listenerWaiting==0||speakerActive > 0 || listenerActive > 0 || messageActive){
+			toSpeak.sleep();
+		}
+		speakerWaiting--;	
+		speakerActive++;
+		ourLock.release();
+		
+		message = word;		//Transfer word to message
+		
+		ourLock.acquire();	//Reacquire ourLock to decrement the active speakers and have an activeMessage
+		messageActive = true;
+		speakerActive--;
+		toListen.wake();	//Wake up a listener so it can listen to the word
+		ourLock.release();
+	}
+	
+	public int listener(){
+		ourLock.acquire();
+		listenerWaiting++;
+		while(speakerWaiting > 0 && listenerActive < 1 && speakerActive < 1 ){//wake up a speaker if there is one
+			toSpeak.wake();
+		}
+		if(!messageActive){ 	//listener will sleep if theres no message to pick up
+			toListen.sleep();
+		}
+		listenerWaiting--;
+		listenerActive++;
+		ourLock.release();
+		
+		int returnMessage = message; //moved to a local variable so we can return 
+		
+		ourLock.acquire();
+		messageActive = false;		//message got picked up
+		listenerActive--;
+		if(speakerWaiting>0){
+			toSpeak.wake();
+		}
+		ourLock.release();
+		
+		return returnMessage;
+	}
+
+
 }
