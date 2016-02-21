07954a27b5c11daa071ad45d8d53d01d1bf196e9
cephaswilco <davidvallieres@gmail.com>
2016-02-21 18:48:20 -0500

    Sunday 6:48pm selfTest STuff
    Added selftests and the classes for testing

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -22,13 +22,67 @@ public class Condition2 {
      *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
     public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
+		this.conditionLock = conditionLock;
     }
     
-   
-    
+	
+   	private ThreadQueue waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
+    private Lock conditionLock;
+	private boolean isNext = false; // boolean flag for wakeall(), makes sure something is in queue.
+	
+	
     public static void selfTest() {
-    	
+		
+		
+		Lib.debug('e',"\n\n\n --------------------------------- !Entering Condition2 SelfTest! -------------------------------");
+		
+		// Tests to see that I can put threads to sleep and they exist on the threadqueue within Condition2
+		Lib.debug('e',"\n\n\n ------Tests to see that I can put threads to sleep and they exist on the threadqueue within Condition2-----\n");
+		Lock l = new Lock();
+    	Condition2 c2 = new Condition2(l);
+		
+		// create 4 new threads
+		KThread thread1 = new KThread (new TestThread(c2, l, "thread1"));
+		KThread thread2 = new KThread (new TestThread(c2, l, "thread2"));
+		KThread thread3 = new KThread (new TestThread(c2, l, "thread3"));
+		KThread thread4 = new KThread (new TestThread(c2, l, "thread4"));
+		
+		// 2 new threads
+		Lib.debug('e',"\n   // 2 new threads  \n");
+		thread1.fork();
+		thread1.setName("thread1");
+		KThread.currentThread().yield();
+		
+		thread2.fork();
+		thread2.setName("thread2");
+		KThread.currentThread().yield();
+		
+		// Continues Tests to make sure wake() function works.
+			//wake nextThread
+		Lib.debug('e',"\n\n\n ------// Continues Tests to make sure wake() function works.-----\n");
+			Lib.debug('e',"\n   //wake nextThread  \n");
+		l.acquire();
+		c2.wake();
+		l.release();
+		
+		// Continues Tests to make sure wakeall() function works.
+		Lib.debug('e',"\n\n\n ------// Continues Tests to make sure wakeall() function works.-----\n");
+			
+			// 2 new threads
+			Lib.debug('e',"\n   // 2 new threads  \n");
+			thread3.fork();
+			thread3.setName("thread3");
+			KThread.currentThread().yield();
+			
+			thread4.fork();
+			thread4.setName("thread4");
+			KThread.currentThread().yield();
+		
+		//wakeAll threads
+		Lib.debug('e',"\n   //wakeAll threads  \n");
+		l.acquire();
+		c2.wakeAll();
+		Lib.debug('e'," --------------------------------- !Exiting Condition2 SelfTest! ------------------------------- \n\n\n");
         }
 
     /**
@@ -38,17 +92,30 @@ public class Condition2 {
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
     public void sleep() {
-		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-		Machine.interrupt().disable();
-		conditionLock.release();
 		
-		KThread.currentThread().sleep();
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		boolean iStat = Machine.interrupt().disable();
+		
+		conditionLock.release();
+
+		if(Communicator.supressDebug == false)
+			Lib.debug('e', KThread.currentThread().getName() + " is sleeping now!");
+	
+		isNext = true; // boolean flag for wakeall(), makes sure something is in queue.
 		waitQueue.waitForAccess(KThread.currentThread());
 		
-		conditionLock.release();
-	
+		if(Communicator.supressDebug == false){
+			Lib.debug('e', "\n Current Sleeping threads: \n" );
+			waitQueue.print(); // This code is here for debug purposes, i'd much rather use a Lib.debug, but I'm not rewriting other lc
+			Lib.debug('e', "\n" );
+		}
+		
+		KThread.currentThread().sleep();
+		
 		conditionLock.acquire();
-		Machine.interrupt().enable();
+		
+		Machine.interrupt().restore(iStat);
+	
     }
 
     /**
@@ -56,35 +123,73 @@ public class Condition2 {
      * current thread must hold the associated lock.
      */
     public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-	Machine.interrupt().disable();
-	KThread thread = waitQueue.nextThread();
-	
-	if (thread != null)
-		 thread.ready();
-	
-	Machine.interrupt().enable();
+		
+		
+				Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+				boolean iStat = Machine.interrupt().disable();
+				
+				KThread kthread = waitQueue.nextThread();
+				
+				if(kthread != null){
+					isNext = true; // boolean flag for wakeall(), makes sure something is in queue.
+					if(Communicator.supressDebug == false){
+						Lib.debug('e',KThread.currentThread().getName() + " is awoken now! \n");
+						Lib.debug('e', "Threads still on queue: \n" );
+						waitQueue.print();
+						Lib.debug('e', "\n" );
+					}
+					kthread.ready();
+				}else{
+					isNext = false; // boolean flag for wakeall(), makes sure something is in queue.
+				}
+				Machine.interrupt().restore(iStat);
+				
     }
 
     
-    
     /**
      * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
+     * thread must hold the associated lock. Uses Wake() function as many times as
+	 * there are threads on the queue.
      */
+	 
     public void wakeAll() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-		Machine.interrupt().disable();
-		KThread thread = waitQueue.nextThread();
-		while (thread != null) {
-			thread.ready();
-			thread = waitQueue.nextThread();
+		boolean iStat = Machine.interrupt().disable();
+			
+			wake();
+			
+		while(isNext == true){
+			wake();
 		}
 		
-		Machine.interrupt().enable();
+		Machine.interrupt().restore(iStat);
 	}
+
+	
+	
+	
+		 private static class TestThread implements Runnable{
+			 
+			 Lock lock;
+			 Condition2 c2;
+			 String name;
+			 public TestThread(Condition2 c2, Lock l, String name ){
+				 this.lock = l;
+				 this.c2 = c2;
+				 this.name = name;
+			 }
+			 
+			public void run(){
+				
+				
+				Lib.debug('e', " \n " + name + " is being put to sleep if it can acquire lock.");
+				lock.acquire();
+				c2.sleep();
+				lock.release();
+				Lib.debug('e', name + " is awake.");
+				
+			}
+		}
+	 }
 	 
-	 
-	 private ThreadQueue waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
-     private Lock conditionLock;
-}
