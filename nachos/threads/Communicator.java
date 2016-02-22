31f8f205c829b8befda668b6e602ef2a9d0fc3ee
ArlyMathiasen <arlym803@gmail.com>
2016-02-22 09:30:07 -0500

    Improved debug display
    +improved clarity in Commuicator debug output
    +improve deug display error fix in Condition2

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -92,82 +92,87 @@ public class Communicator {
 	private static class testSpeaker implements Runnable{ //
 		private Communicator ourCom;
 		private int message;
+		private String name;
 		
-		public testSpeaker(Communicator comm, int msg){
+		public testSpeaker(Communicator comm, int msg, String name){
 			ourCom = comm;
 			message = msg;
+			this.name = name;
 		}
 		public void run(){
-			Lib.debug('u',"\nCurrently Speaking a message:"+message);
+			Lib.debug('u',"\n"+name+" Speaking message:"+message);
 			ourCom.speak(message);
 		}
 	}
 	private static class testListener implements Runnable{
 		private Communicator ourCom;
-		
-		public testListener(Communicator comm){
+		private String name;
+	
+		public testListener(Communicator comm, String name){
 			ourCom = comm;
+			this.name = name;
 		}
 		public void run(){
-			Lib.debug('u',"\nCurrently Listening to a message:"+ourCom.listen());
+			Lib.debug('u', "\n"+name+" Listening for message");
+			Lib.debug('u',"\n"+name+" heard message:"+ourCom.listen());
 		}
 		
 	}
 	public static void selfTest(){
+		Lib.debug('u',"\nBegin Communicator selfTest");
 		Communicator ourCom = new Communicator(); //Communicator for our testcase
-		Condition2.supressDebug = true;
-		Lib.debug('u',"\nAttempt 1 : Queued a Speaker with message = 1 then a Listener");
-		KThread t1 = new KThread(new testSpeaker(ourCom,1)); 
-		t1.setName("CommunicatorThread1");
+		Lib.debug('u',"\nTest 1 : Queued a Speaker with message = 1 then a Listener");
+		KThread t1 = new KThread(new testSpeaker(ourCom,1,"SpeakerThread1")); 
+		t1.setName("SpeakerThread1");
 		t1.fork();
-		KThread t2 = new KThread(new testListener(ourCom));
-		t2.setName("CommunicatorThread2");		
+		KThread t2 = new KThread(new testListener(ourCom,"ListenerThread1"));
+		t2.setName("ListenerThread1");		
 		t2.fork();
 		t1.join();//queueing the speaker
 		t2.join();//queueing the listener
-		Lib.debug('u',"\nAttempt 2 : Queued a Listener then a Speaker with a message = 2");
-		KThread t3 = new KThread(new testListener(ourCom));
-		t3.setName("CommunicatorThread3");		
+		Lib.debug('u',"\nTest 1 end\nTest 2 : Queued a Listener then a Speaker with a message = 2");
+		KThread t3 = new KThread(new testListener(ourCom,"ListenerThread2"));
+		t3.setName("ListenerThread2");		
 		t3.fork();
-		KThread t4 = new KThread(new testSpeaker(ourCom,2));
-		t4.setName("CommunicatorThread4");		
+		KThread t4 = new KThread(new testSpeaker(ourCom,2,"SpeakerThread2"));
+		t4.setName("SpeakerThread2");		
 		t4.fork();
 		t3.join();//queueing the listener
 		t4.join();//queueing the speaker
-		Lib.debug('u',"\nAttempt 3: Queued two Speakers with message = 3 and 4, then two Listeners");
-		KThread t5 = new KThread(new testSpeaker(ourCom,3));
-		KThread t6 = new KThread(new testSpeaker(ourCom,4));
-		t5.setName("CommunicatorThread5");		
+		Lib.debug('u',"\nTest 2 end\nTest 3: Queued two Speakers with message = 3 and 4, then two Listeners");
+		KThread t5 = new KThread(new testSpeaker(ourCom,3,"SpeakerThread3"));
+		KThread t6 = new KThread(new testSpeaker(ourCom,4,"SpeakerThread4"));
+		t5.setName("SpeakerThread3");		
 		t5.fork();
-		t6.setName("CommunicatorThread6");		
+		t6.setName("SpeakerThread4");		
 		t6.fork();
-		KThread t7 = new KThread(new testListener(ourCom));
-		KThread t8 = new KThread(new testListener(ourCom));
-		t7.setName("CommunicatorThread7");		
+		KThread t7 = new KThread(new testListener(ourCom,"ListenerThread3"));
+		KThread t8 = new KThread(new testListener(ourCom,"ListenerThread4"));
+		t7.setName("ListenerThread3");		
 		t7.fork();
-		t8.setName("CommunicatorThread8");		
+		t8.setName("ListenerThread4");		
 		t8.fork();
 		t5.join();//queueing the speaker #1
 		t6.join();//queueing the speaker #2
 		t7.join();//queueing the listener #1
 		t8.join();//queueing the listener #2
-		Lib.debug('u',"\nAttempt 4:: Queued two Listeners then two Speakers  with message = 5 and 6");
-		KThread t9 = new KThread(new testListener(ourCom));
-		KThread t10 = new KThread(new testListener(ourCom));
-		t9.setName("CommunicatorThread9");		
+		Lib.debug('u',"\nTest 3 end\nTest 4: Queued two Listeners then two Speakers  with message = 5 and 6");
+		KThread t9 = new KThread(new testListener(ourCom,"ListenerThread5"));
+		KThread t10 = new KThread(new testListener(ourCom,"LestenerThread6"));
+		t9.setName("ListenerThread5");		
 		t9.fork();
-		t10.setName("CommunicatorThread10");		
+		t10.setName("ListenerThread6");		
 		t10.fork();
-		KThread t11 = new KThread(new testSpeaker(ourCom,5));
-		KThread t12 = new KThread(new testSpeaker(ourCom,6));
-		t11.setName("CommunicatorThread11");		
+		KThread t11 = new KThread(new testSpeaker(ourCom,5,"SpeakerThread5"));
+		KThread t12 = new KThread(new testSpeaker(ourCom,6,"SpeakerThread6"));
+		t11.setName("SpeakerThread5");		
 		t11.fork();
-		t12.setName("CommunicatorThread12");		
+		t12.setName("SpeakerThread6");		
 		t12.fork();
 		t9.join(); //queueing the listener #1
 		t10.join();//queueing the listener #2
 		t11.join();//queueing the speaker #1
 		t12.join();//queueing the speaker #2
-		Condition2.supressDebug = false;
+		Lib.debug('u',"\nTest 4 end\nCommunicator selfTest end");
 	}
 }
